bdedb669c0b71de106e0a7f7ef474cf16035e93f
Masahiro Yamauchi <sgt.yamauchi@gmail.com>
2019-04-08 18:18:52 +0900

    Show conditions

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ let app = new Vue({
   el: '#app',
   data: {
     dividers: [
-      { value: 0, enable: true },
-      { value: 10, enable: false },
-      { value: 20, enable: true },
-      { value: 30, enable: false },
-      { value: 40, enable: false },
-      { value: 50, enable: false }
+      { value: 0, enabled: true },
+      { value: 10, enabled: false },
+      { value: 20, enabled: true },
+      { value: 30, enabled: false },
+      { value: 40, enabled: false },
+      { value: 50, enabled: false }
     ],
     grids: [
       { value: 0 },
@@ -17,11 +17,40 @@ let app = new Vue({
       { value: 40 },
       { value: 50 },
     ],
+    min: 0,
+    max: 50,
+    conditions: [],
     message: 'Vue multi divider!!'
   },
+  created(){
+    this.getConditions();
+  },
   methods: {
+    conditionString(condition){
+      let res = '';
+      if(condition.min){
+        res += `${condition.min} 以上 `;
+      }
+      if(condition.max){
+        res += `${condition.max} 未満`;
+      }
+      return res;
+    },
+    getConditions(){
+      const conditions = [];
+      const filteredValues = this.dividers.filter(div => div.enabled).map(div => div.value);
+      if(filteredValues.length > 0 && filteredValues[filteredValues.length - 1] < this.max) {
+        filteredValues.push(this.max);
+      }
+      this.conditions = filteredValues.map((val, index, arr) => {
+        const min = (this.min == val) ? null : val;
+        const max = (index < filteredValues.length - 1) ? arr[index+1] : null;
+        return { min, max }
+      });
+    },
     toggleGrid(index) {
-      this.dividers[index].enable = !this.dividers[index].enable;
+      this.dividers[index].enabled = !this.dividers[index].enabled;
+      this.getConditions();
     }
   }
 })
