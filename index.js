8063504ffcf027f4fd49a911400fb9a28b61f5da
Masahiro Yamauchi <sgt.yamauchi@gmail.com>
2019-04-08 15:23:29 +0900

    Enable to toggle a grid to add a divider

--- index.js
+++ index.js
@@ -9,6 +9,19 @@ let app = new Vue({
       { enable: false },
       { enable: true, last: true }
     ],
+    grids: [
+      { value: 0 },
+      { value: 10 },
+      { value: 20 },
+      { value: 30 },
+      { value: 40 },
+      { value: 50, last: true },
+    ],
     message: 'Vue multi divider!!'
+  },
+  methods: {
+    toggleGrid(index) {
+      this.dividers[index].enable = !this.dividers[index].enable;
+    }
   }
 })
