766a8f7aa0e6e11fa46afb6b6bb5fcb20d75e092
Masahiro Yamauchi <sgt.yamauchi@gmail.com>
2019-04-08 14:22:42 +0900

    Tune the last element

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 let app = new Vue({
   el: '#app',
   data: {
+    dividers: [
+      { enable: true },
+      { enable: false },
+      { enable: true },
+      { enable: false },
+      { enable: false },
+      { enable: true, last: true }
+    ],
     message: 'Vue multi divider!!'
   }
 })
